20d55d22a0ebdc44aa4f2797d56112f2f949fe3b
vn2020 <vishnu.venkata1992@gmail.com>
2024-06-15 00:30:17 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    organization = "addu"
+
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
